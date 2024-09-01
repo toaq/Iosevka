178d42b29eb94c43c4fdd90185ec83368a00818f
Robin <robin@robin.town>
2024-09-01 18:00:55 -0400

    Add a nix flake heehee

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            nodejs_latest
+          ];
+        };
+      });
+}
