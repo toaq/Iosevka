656d9e170bcf8960184780d3f1b7f03e6435f5b3
Robin <robin@robin.town>
2024-09-21 22:11:19 -0400

    Set up a Nix package

--- flake.nix
+++ flake.nix
@@ -2,12 +2,23 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
-      in {
+      in rec {
         devShells.default = pkgs.mkShellNoCC {
-          buildInputs = with pkgs; [
-            nodejs_latest
-            ttfautohint
-          ];
+          inherit (defaultPackage) nativeBuildInputs;
+        };
+        defaultPackage = pkgs.buildNpmPackage {
+          name = "iosevka-toaq";
+          src = self;
+          nativeBuildInputs = [ pkgs.nodejs_latest pkgs.ttfautohint ];
+          npmDepsHash = "sha256-xw0GA1aIA/J5hfLQBSE+GJzXfbfWQI2k2pYdenlM9NY=";
+          buildPhase = ''
+            npm run build -- contents::IosevkaToaq contents::IosevkaToaqSlab contents::IosevkaToaqAile contents::IosevkaToaqEtoile
+          '';
+          installPhase = ''
+            mkdir -p $out/share/fonts/{truetype,woff2}
+            mv dist/*/TTF/*.ttf $out/share/fonts/truetype
+            mv dist/*/WOFF2/*.woff2 $out/share/fonts/woff2
+          '';
         };
       });
 }
